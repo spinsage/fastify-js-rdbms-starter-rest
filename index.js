61196959d227d75c4ddc2fe67da462c933cded2a
shashi <shashi@spinsage.com>
2021-12-27 14:33:57 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const fastify = require("fastify");
+global.__basedir = __dirname
+
+const serverConfig = {
+    logger: {
+        prettyPrint: true
+    }
+}
+
+const server = fastify(serverConfig)
+
+server.register(require('./src/plugins/config'));
+server.register(require('./src/plugins/db'));
+server.register(require('./src/plugins/dbModels'));
+server.register(require('./src/routes'));
+
+const startSever = async () => {
+    await server.ready()
+    server.listen(process.env.SERVER_PORT, (err) => {
+        if (err) {
+            server.log.error(err);
+            process.exit(1);
+        }
+    });
+}
+
+startSever();
